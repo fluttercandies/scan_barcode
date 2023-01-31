753f728b6b8a789c6531f7d6de5ff49a476e5185
caijinglong-b-imac <cjl_spy@163.com>
2023-01-31 11:49:57 +0800

    :construction: Update example

--- README.md
+++ README.md
@@ -1,25 +1,82 @@
-# qr_camera
+# scan_barcode
 
-The plugin is a plugin for scanning QR codes. Only support android and ios.
+The plugin is a plugin for scanning barcodes. Only support android and ios. Because the plugin is based on mlkit.
 
 ## Getting Started
 
 ```yaml
 dependencies:
-  qr_camera: ^0.1.0
+  scan_barcode: ^0.1.0
 ```
 
-See the package [qr_camera](https://pub.dev/packages/qr_camera) for more version.
+See the package [scan_barcode](https://pub.dev/packages/scan_barcode) for more version.
 
-`import 'package:qr_camera/qr_camera.dart';`
+`import 'package:scan_barcode/scan_barcode.dart';`
 
 The plugin is developed by version 3.1.0 of flutter.
 
 ## Example
 
-See the [example](./example) for a complete sample app using qr_camera.
+See the [example](./example/lib/examples) for more examples.
 
 ```dart
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:scan_barcode/scan_barcode.dart';
+
+class ShowDialogExample extends StatelessWidget {
+  const ShowDialogExample({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BarcodeScanPage(
+      title: 'Show dialog when scanned',
+      onHandleBarcodeList: (List<Barcode> barCode) async {
+        if (barCode.isEmpty) return;
+        await showBarcodeListDialog(context, barCode); // The await is important, if you don't await, multiple dialogs will be shown.
+      },
+    );
+  }
+
+  Future<void> showBarcodeListDialog(
+      BuildContext context, List<Barcode> barCode) async {
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Barcode list'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            for (final barcode in barCode)
+              ListTile(
+                title: Text(barcode.rawValue ?? ''),
+                subtitle:
+                    Text('type: ${barcode.type}, format: ${barcode.format}'),
+                trailing: IconButton(
+                  icon: const Icon(Icons.copy),
+                  onPressed: () {
+                    Clipboard.setData(
+                      ClipboardData(text: barcode.rawValue ?? ''),
+                    );
+                  },
+                ),
+              ),
+          ],
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+}
 ```
 
 ## dependencies
--- example/lib/examples/show_dialog_example.dart
+++ example/lib/examples/show_dialog_example.dart
@@ -13,40 +13,45 @@ class ShowDialogExample extends StatelessWidget {
       title: 'Show dialog when scanned',
       onHandleBarcodeList: (List<Barcode> barCode) async {
         if (barCode.isEmpty) return;
-        await showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: const Text('Barcode list'),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                for (final barcode in barCode)
-                  ListTile(
-                    title: Text(barcode.rawValue ?? ''),
-                    subtitle: Text(
-                        'type: ${barcode.type}, format: ${barcode.format}'),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.copy),
-                      onPressed: () {
-                        Clipboard.setData(
-                          ClipboardData(text: barcode.rawValue ?? ''),
-                        );
-                      },
-                    ),
-                  ),
-              ],
-            ),
-            actions: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('OK'),
-              ),
-            ],
-          ),
-        );
+        await showBarcodeListDialog(context, barCode); // The await is important, if you don't await, multiple dialogs will be shown.
       },
+    );
+  }
+
+  Future<void> showBarcodeListDialog(
+      BuildContext context, List<Barcode> barCode) async {
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Barcode list'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            for (final barcode in barCode)
+              ListTile(
+                title: Text(barcode.rawValue ?? ''),
+                subtitle:
+                    Text('type: ${barcode.type}, format: ${barcode.format}'),
+                trailing: IconButton(
+                  icon: const Icon(Icons.copy),
+                  onPressed: () {
+                    Clipboard.setData(
+                      ClipboardData(text: barcode.rawValue ?? ''),
+                    );
+                  },
+                ),
+              ),
+          ],
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      ),
     );
   }
 }
